548b0a12518b2d82f279fb60a4bb801f3ef0c8ac
haiwulei <1021391538@qq.com>
2018-05-31 14:16:50 +0800

    api

--- client/app/core/services/global.ts
+++ client/app/core/services/global.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
   // public apiDomain: String = 'http://192.168.1.39:8001';  // Mainnet
   // public apiDotest: String = 'http://192.168.1.39:8002';  // Testnet
-  public apiDomain: String = 'https://api.blolys.com';
+  public apiDomain: String = 'https://api.iwallic.com';
   // public apiDomain: String = 'http://192.168.1.90:8080';
   // public apiDomain: String = 'http://149.28.17.215:8080';
   constructor() { }
