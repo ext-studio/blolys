8b1fc5f44a566bbd2864deadd959259438544c2d
haiwulei <1021391538@qq.com>
2018-05-31 14:10:58 +0800

    ico, api

--- client/app/core/services/global.ts
+++ client/app/core/services/global.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
   // public apiDomain: String = 'http://192.168.1.39:8001';  // Mainnet
   // public apiDotest: String = 'http://192.168.1.39:8002';  // Testnet
-  public apiDomain: String = 'http://47.75.174.167:8001';
+  public apiDomain: String = 'https://api.blolys.com';
   // public apiDomain: String = 'http://192.168.1.90:8080';
   // public apiDomain: String = 'http://149.28.17.215:8080';
   constructor() { }
