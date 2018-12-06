9dfc1316d8a490df18b091482e17a197e5a4478e
haiwulei <1021391538@qq.com>
2018-12-06 11:11:52 +0800

    update main api

--- client/app/core/services/global.ts
+++ client/app/core/services/global.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
     // public apiDomain: String = 'http://192.168.1.39:8080';  // Mainnet
     // public apiDotest: String = 'http://192.168.1.39:8002';  // Testnet
-    public apiDomain: String = 'https://api.iwallic.com';
+    public apiDomain: String = 'https://api.iwallic.forchain.info';
     public apiDotest: String = 'https://teapi.iwallic.com';
     // public apiDomain: String = 'http://149.28.17.215:8080';
     public netDomain: String = 'mainnet';
